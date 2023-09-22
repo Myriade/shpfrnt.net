8d42ec19fe3442116ce535e1ea73229c231353a3
Myriam <myriambizier@gmail.com>
2023-09-22 11:29:29 -0400

    Site Header WIP

--- src/views/siteHeader/siteHeader.html.js
+++ src/views/siteHeader/siteHeader.html.js
@@ -1,6 +1,13 @@
 export default function () {
 return `
-  
+
   <h1>Shopfront</h1>
+  <nav>
+    <a class="checkout">Checkout</a>
+    <a class="cart">Cart</a>
+  </nav>
+  <aside>
+    <p>!!**Sale just got better**!!</p>
+  </aside>
   
 `}

5749b22dee5388216f108704209411faff0a4de4
Myriam <myriambizier@gmail.com>
2023-09-28 22:28:29 -0400

    Product iteration in siteMain

--- src/views/siteMain/siteMain.html.js
+++ src/views/siteMain/siteMain.html.js
@@ -1,8 +1,31 @@
+import data from '../../../data.json';
+
+const products = data.products;
+
+let productIteration = '';
+
+for (const product of products) {
+  const cardTemplate = `
+    <div class="product">
+      <figure>
+        <img src="/media/products/images/${product.imageFileName}" alt="${product.title}">
+      </figure>
+      <h2>${product.title}</h2>
+      <p>$${product.regularPrice}</p>
+      <p>$${product.discountedPrice}</p>
+      <p>Rating: ${product.rating}</p>
+      <p>Reviews: ${product.numberOfReviews}</p>
+    </div>
+  `;
+  productIteration += cardTemplate;
+}
+
 export default function () {
+  
 return `
   
-  <figure>
-    <img src="images/example-item.png" alt="">
-  </figure>
+  <section id='products'>
+    ${productIteration}
+  </section>
   
 `}

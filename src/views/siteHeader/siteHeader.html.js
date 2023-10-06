8403dd003acea753a953872a8fa6e2df31b12acd
MyriamB sur gonw <myriam@myriadeweb.com>
2023-10-06 15:08:26 -0400

    wip

--- src/views/siteHeader/siteHeader.html.js
+++ src/views/siteHeader/siteHeader.html.js
@@ -4,8 +4,8 @@ export default function () {
 return `
   <h1>Shopfront</h1>
   <nav>
-    <a href="#" class="checkout">Checkout</a>
-    <a href="#" class="cart"><span id="cart__amount"></span></a>
+    <a href="#" class="checkout open-modal">Checkout</a>
+    <a href="#" class="cart open-modal"><span id="cart__amount"></span></a>
   </nav>
   <aside>
     <p id="headerBannerPhrase">${currentPhrase}</p>

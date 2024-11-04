74f336341c81088a914427cce55d08a2756108ad
CNZKMP <cinziacampolese@gmail.com>
2024-11-04 15:22:57 +0100

    about update

--- src/views/aboutModal/aboutModal.html.js
+++ src/views/aboutModal/aboutModal.html.js
@@ -2,7 +2,7 @@ export default function () {
 return `
   <div class="modal-content">
     <h3>About</h3>
-    <p>Project by C&L collective, the project funded by the Canada Council for the Arts - Digital Now</p>
+    <p>Project by C&L Collective, funded by the Canada Council for the Arts – Digital Now</p>
     <div class="button button--pill button--modal close-modal">Return</div>
   </div>
 `}
